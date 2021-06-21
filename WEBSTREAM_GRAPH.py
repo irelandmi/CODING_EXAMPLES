6617d96e8b8cf5e58ead8806874d0d75f6f1ab11
irelandmi <isaacmireland@gmail.com>
2021-06-21 23:44:19 +0100

    Add files via upload

---
+++ WEBSTREAM_GRAPH.py
@@ -0,0 +1,31 @@
+#%%
+
+import numpy as np
+import json
+from datetime import datetime
+import pandas as pd
+import time
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib.animation import FuncAnimation
+import csv
+import pandas as pd
+#%%
+data = pd.read_csv('prices_flat.csv')
+
+#%%
+plt.style.use('fivethirtyeight')
+
+def animate(i):
+    data = pd.read_csv('prices_flat.csv')
+    x = data.index[-1000:]
+    y1 = data['price'].iloc[-1000:]
+
+    plt.cla()
+    plt.plot(x, y1, label='Price')
+    plt.tight_layout()
+
+ani = FuncAnimation(plt.gcf(), animate, interval=100)
+
+plt.tight_layout()
+plt.show()
